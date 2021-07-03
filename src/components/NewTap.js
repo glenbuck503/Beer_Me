70e5ea1958d9a709ff61c71858aa438ac873c245
glenbuck503 <glenbuck503@gmail.com>
2021-07-02 20:07:07 -0700

    handleNewTapSubmission and ReactFragment

---
+++ src/components/NewTap.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+function NewTap(props) {
+  const tap = {};
+  tap.id = v4();
+
+  function handleNewTapSubmission(event) {
+    event.preventDefualt();
+    props.onNewTapCreation({names: event.target.names.value, pint: 124, id :v4()})
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+      formSubmissionHandler={handleNewTapSubmission}
+      buttonText = "Add Tap!" />
+      </React.Fragment>
+  )
+}
