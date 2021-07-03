fb015e678ebc9d8c1389c06d8133b517c9f9ba83
glenbuck503 <glenbuck503@gmail.com>
2021-07-02 21:06:23 -0700

    able to display taps to list

--- src/components/NewTap.js
+++ src/components/NewTap.js
@@ -3,24 +3,24 @@ import { v4 } from "uuid";
 import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
 
-function NewTap(props) {
-  const tap = {};
-  tap.id = v4();
+function NewTap(props){
+const tap = {};
+tap.id = v4();
 
-  function handleNewTapSubmission(event) {
-    event.preventDefualt();
-    props.onNewTapCreation({names: event.target.names.value, pint: 124, id :v4()})
-  }
-
-  return (
-    <React.Fragment>
-      <ReusableForm
-      formSubmissionHandler={handleNewTapSubmission}
-      buttonText = "Add Tap!" />
-      </React.Fragment>
-  );
+function handleNewTapSubmission(event) {
+  event.preventDefault();
+  props.onNewTapCreation({names: event.target.names.value, pint: 124, id: v4()});
 }
+return (
 
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleNewTapSubmission}
+        buttonText="Add Tap!" />
+    </React.Fragment>
+  );
+  }
+  
   NewTap.propTypes = {
     onNewTapCreation: PropTypes.func
   };
