2a63e4ad6352e0fdb771b902852dadb15190c2a6
glenbuck503 <glenbuck503@gmail.com>
2021-07-02 20:02:38 -0700

    functionality to get to adding a tap works correctly. Does not display form yet

--- src/components/TapDetail.js
+++ src/components/TapDetail.js
@@ -2,13 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function TapDetail(props){
-  const {tap, onClickingDelete, onClickindEdit,onSubtractPint} = props;
+  const {tap, onClickingDelete, onClickingEdit,onSubtractPint} = props;
 
   return (
     <React.Fragment>
       <h1>Tap Detail</h1>
       <h3>{tap.names}</h3>
       <h3>{tap.pint}</h3>
+      <button onClick={ ()=> onClickingEdit(props.pint) }>Update Tap</button> 
+      
+      <button onClick={()=> onClickingDelete(tap.id) }>Remove Tap</button>
+
+      <button onClick={() => onSubtractPint(tap.id)}>Drink</button>
       </React.Fragment>
   );
 }
+
+
+TapDetail.propTypes = {
+  tap: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func,
+  onSubtractPint: PropTypes.func
+};
+
+export default TapDetail;
