06acafd52ba2e7b6cf9bc0224f9c0e9efb88bc41
glenbuck503 <glenbuck503@gmail.com>
2021-07-02 19:48:09 -0700

    add first function to TapList

---
+++ src/components/TapDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tap from './Tap';
+import PropTypes from 'prop-types';
+
+
+function TapList(props){
+  return (
+    <React.Fragment>
+      </React.Fragment>
+
+  );
+}
