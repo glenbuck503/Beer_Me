86854f9517113ad1b19936a1ad026827ede1e675
glenbuck503 <glenbuck503@gmail.com>
2021-07-02 20:23:09 -0700

    add gif

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
-import tapsImage from "./../img/beer.jpeg"
+import tapsImage from "./../img/suds.gif"
 
 function Header() {
   return (
     <React.Fragment>
       <h1>Tap Room</h1>
       <img src={tapsImage} id="beerPic" />
+   
+      
       </React.Fragment>
   );
 }
